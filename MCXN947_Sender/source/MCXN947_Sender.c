eb0a0e5a6b4ee5423e4d4533070951cb57104665
RCTd <rafytudu@gmail.com>
2026-01-23 12:58:23 +0200

    Sender init refactor + working input FIFO

--- MCXN947_Sender/source/MCXN947_Sender.c
+++ MCXN947_Sender/source/MCXN947_Sender.c
@@ -13,36 +13,82 @@
 #include "app.h"
 
 /*******************************************************************************
+ * Definitions & FIFO Settings
+ ******************************************************************************/
+#define FIFO_SIZE 16 /* Must be power of 2 */
+
+typedef struct
+{
+    uint32_t id;
+    uint8_t dataByte;
+} can_msg_t;
+
+/*******************************************************************************
  * Variables
  ******************************************************************************/
-volatile bool g_reqSendButton3Msg = false;
-volatile bool g_reqSendButton2Msg = false;
-volatile bool g_reqSendTimerMsg = false;
 
-/* FlexCAN Handle for Non-Blocking Transfers */
+/* FIFO Variables */
+volatile can_msg_t fifo_buffer[FIFO_SIZE];
+volatile uint8_t fifo_head = 0;
+volatile uint8_t fifo_tail = 0;
+
+/* FlexCAN Handle and Variables */
 flexcan_handle_t flexcanHandle;
 flexcan_fd_frame_t txFrame;
-flexcan_mb_transfer_t txXfer; /* Transfer structure required for Non-Blocking API */
-volatile bool txComplete = false;
+flexcan_mb_transfer_t txXfer;
+volatile bool txComplete = true;
 volatile bool txError = false;
 
 static ctimer_match_config_t matchConfig0;
 
 /*******************************************************************************
+ * FIFO Helper Functions
+ ******************************************************************************/
+bool FIFO_Push(uint32_t id, uint8_t data)
+{
+    uint8_t next_head = (fifo_head + 1) % FIFO_SIZE;
+
+    if (next_head == fifo_tail)
+    {
+        return false; /* Buffer Full */
+    }
+
+    fifo_buffer[fifo_head].id = id;
+    fifo_buffer[fifo_head].dataByte = data;
+    fifo_head = next_head;
+    return true;
+}
+
+bool FIFO_Pop(can_msg_t *msg)
+{
+    if (fifo_head == fifo_tail)
+    {
+        return false; /* Buffer Empty */
+    }
+
+    *msg = fifo_buffer[fifo_tail];
+    fifo_tail = (fifo_tail + 1) % FIFO_SIZE;
+    return true;
+}
+
+bool FIFO_IsEmpty(void)
+{
+    return (fifo_head == fifo_tail);
+}
+
+/*******************************************************************************
  * Callback Function
  ******************************************************************************/
-/* FIXED: result parameter changed to uint64_t to match driver signature */
 static void flexcan_callback(CAN_Type *base, flexcan_handle_t *handle, status_t status, uint64_t result, void *userData)
 {
     if (status == kStatus_FLEXCAN_TxIdle)
     {
         txComplete = true;
     }
-    /* FIXED: Removed kStatus_FLEXCAN_BusOff as it was undeclared.
-       Bus Off usually comes as kStatus_FLEXCAN_ErrorStatus with flags in 'result' */
     else if (status == kStatus_FLEXCAN_ErrorStatus)
     {
         txError = true;
+        txComplete = true;
     }
 }
 
@@ -57,19 +103,19 @@ void BOARD_SW3_IRQ_HANDLER(void)
     if ((interruptFlags & (1U << BOARD_SW3_GPIO_PIN)) != 0)
     {
         GPIO_GpioClearInterruptFlags(BOARD_SW3_GPIO, 1U << BOARD_SW3_GPIO_PIN);
-        g_reqSendButton3Msg = true;
 
-        /* Reset Timer */
         CTIMER_StopTimer(CTIMER);
         CTIMER_Reset(CTIMER);
         CTIMER_StartTimer(CTIMER);
+
+        FIFO_Push(TX_MSG_ID_TIMER, 0x33);
     }
 
     /* SW2 (P0_23) */
     if ((interruptFlags & (1U << BOARD_SW2_GPIO_PIN)) != 0)
     {
         GPIO_GpioClearInterruptFlags(BOARD_SW2_GPIO, 1U << BOARD_SW2_GPIO_PIN);
-        g_reqSendButton2Msg = true;
+        FIFO_Push(TX_MSG_ID_TIMER, 0x22);
     }
     SDK_ISR_EXIT_BARRIER;
 }
@@ -77,7 +123,7 @@ void BOARD_SW3_IRQ_HANDLER(void)
 void ctimer_match0_callback(uint32_t flags)
 {
     CTIMER_StopTimer(CTIMER);
-    g_reqSendTimerMsg = true;
+    FIFO_Push(TX_MSG_ID_TIMER, 0xBB);
 }
 
 ctimer_callback_t ctimer_callback_table[] = {
@@ -90,13 +136,6 @@ ctimer_callback_t ctimer_callback_table[] = {
 
 static void SendCanMessageNonBlocking(uint32_t id, uint8_t dataByte)
 {
-    /* If a previous transfer is still stuck (rare with callback logic), don't overwrite */
-    if (FLEXCAN_GetMbStatusFlags(EXAMPLE_CAN, 1ULL << TX_MESSAGE_BUFFER_NUM))
-    {
-        LOG_INFO("Busy. Skipping send.\r\n");
-        return;
-    }
-
     txFrame.id     = FLEXCAN_ID_STD(id);
     txFrame.format = (uint8_t)kFLEXCAN_FrameFormatStandard;
     txFrame.type   = (uint8_t)kFLEXCAN_FrameTypeData;
@@ -105,27 +144,25 @@ static void SendCanMessageNonBlocking(uint32_t id, uint8_t dataByte)
     txFrame.length = 1U;
     txFrame.dataWord[0] = CAN_WORD_DATA_BYTE_0(dataByte);
 
-    /* Prepare Transfer Structure */
     txXfer.mbIdx = TX_MESSAGE_BUFFER_NUM;
     txXfer.framefd = &txFrame;
 
     txComplete = false;
     txError = false;
 
-    LOG_INFO("Sending ID: 0x%X... \r\n", id);
+    LOG_INFO("Sending ID: 0x%X Data: 0x%02X \r\n", id, dataByte);
 
-    /* Use Non-Blocking API */
     status_t status = FLEXCAN_TransferFDSendNonBlocking(EXAMPLE_CAN, &flexcanHandle, &txXfer);
 
     if (status != kStatus_Success)
     {
         LOG_INFO("Fail to queue (Stat: %d).\r\n", status);
+        txComplete = true;
     }
 }
 
 static void FLEXCAN_PHY_Config_Safe(void)
 {
-    /* Setup TX MB */
     FLEXCAN_SetFDTxMbConfig(EXAMPLE_CAN, TX_MESSAGE_BUFFER_NUM, true);
 
     /* Wake Transceiver */
@@ -145,11 +182,8 @@ static void FLEXCAN_PHY_Config_Safe(void)
     txXfer.framefd = &txFrame;
 
     txComplete = false;
-
-    /* SEND WITH TIMEOUT PROTECTION */
     FLEXCAN_TransferFDSendNonBlocking(EXAMPLE_CAN, &flexcanHandle, &txXfer);
 
-    /* Wait max 100ms for ACK */
     volatile uint32_t timeout = 1000000;
     while (!txComplete && timeout > 0)
     {
@@ -160,57 +194,59 @@ static void FLEXCAN_PHY_Config_Safe(void)
     {
         LOG_INFO("PHY Config Timeout (No Bus ACK). Aborting...\r\n");
         FLEXCAN_TransferFDAbortSend(EXAMPLE_CAN, &flexcanHandle, TX_MESSAGE_BUFFER_NUM);
+        txComplete = true;
     }
     else
     {
         LOG_INFO("PHY Config ACK Received.\r\n");
     }
 
-    /* Set STB Low (Normal Mode) */
     GPIO_PortClear(EXAMPLE_STB_RGPIO, 1u << EXAMPLE_STB_RGPIO_PIN);
 }
 
 /*******************************************************************************
- * Main
+ * Initialization Function
  ******************************************************************************/
-int main(void)
+static void Init_Peripherals(void)
 {
     flexcan_config_t flexcanConfig;
     ctimer_config_t ctimerConfig;
     gpio_pin_config_t sw_config = { kGPIO_DigitalInput, 0 };
+    flexcan_timing_config_t timing_config;
 
+    /* 1. Board Init */
     BOARD_InitHardware();
-    LOG_INFO("--- No-Hang CAN FD Sender ---\r\n");
+    LOG_INFO("--- No-Hang CAN FD Sender (FIFO Enabled) ---\r\n");
 
-    /* GPIO Init */
+    /* 2. GPIO Init */
     GPIO_SetPinInterruptConfig(BOARD_SW3_GPIO, BOARD_SW3_GPIO_PIN, kGPIO_InterruptFallingEdge);
     GPIO_PinInit(BOARD_SW3_GPIO, BOARD_SW3_GPIO_PIN, &sw_config);
     GPIO_SetPinInterruptConfig(BOARD_SW2_GPIO, BOARD_SW2_GPIO_PIN, kGPIO_InterruptFallingEdge);
     GPIO_PinInit(BOARD_SW2_GPIO, BOARD_SW2_GPIO_PIN, &sw_config);
     EnableIRQ(BOARD_SW3_IRQ);
 
-    /* CTimer Init */
+    /* 3. CTimer Init */
     CTIMER_GetDefaultConfig(&ctimerConfig);
     CTIMER_Init(CTIMER, &ctimerConfig);
+
     matchConfig0.enableCounterReset = true;
     matchConfig0.enableCounterStop = false;
     matchConfig0.matchValue = CTIMER_CLK_FREQ * 1;
     matchConfig0.outControl = kCTIMER_Output_NoAction;
     matchConfig0.outPinInitState = false;
     matchConfig0.enableInterrupt = true;
+
     CTIMER_RegisterCallBack(CTIMER, &ctimer_callback_table[0], kCTIMER_MultipleCallback);
     CTIMER_SetupMatch(CTIMER, CTIMER_MAT0_OUT, &matchConfig0);
     EnableIRQ(CTIMER_IRQ_ID);
 
-    /* FlexCAN Init */
+    /* 4. FlexCAN Init */
     FLEXCAN_GetDefaultConfig(&flexcanConfig);
-//    flexcanConfig.bitRate = 500000U;
     flexcanConfig.bitRateFD = 2000000U;
 #if defined(EXAMPLE_CAN_CLK_SOURCE)
     flexcanConfig.clkSrc = EXAMPLE_CAN_CLK_SOURCE;
 #endif
 
-    flexcan_timing_config_t timing_config;
     memset(&timing_config, 0, sizeof(flexcan_timing_config_t));
     if (FLEXCAN_FDCalculateImprovedTimingValues(EXAMPLE_CAN, flexcanConfig.bitRate, flexcanConfig.bitRateFD,
                                                 EXAMPLE_CAN_CLK_FREQ, &timing_config))
@@ -219,37 +255,47 @@ int main(void)
     }
 
     FLEXCAN_FDInit(EXAMPLE_CAN, &flexcanConfig, EXAMPLE_CAN_CLK_FREQ, BYTES_IN_MB, true);
-
-    /* Create the Handle for Interrupt-driven transfers */
     FLEXCAN_TransferCreateHandle(EXAMPLE_CAN, &flexcanHandle, flexcan_callback, NULL);
 
-    /* Configure PHY safely */
+    /* 5. FlexCAN PHY & MB Setup */
     FLEXCAN_PHY_Config_Safe();
-
-    /* Set up Tx MB */
     FLEXCAN_SetFDTxMbConfig(EXAMPLE_CAN, TX_MESSAGE_BUFFER_NUM, true);
-    /* FIXED: Removed txFrame.mbIdx assignment */
 
-    LOG_INFO("System Ready.\r\n");
+    /* Ensure txComplete starts true for the main loop */
+    txComplete = true;
+
+    LOG_INFO("Initialization Complete. Waiting for events...\r\n");
+}
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
+int main(void)
+{
+    /* Call the consolidated Init function */
+    Init_Peripherals();
+
+    can_msg_t nextMsg;
 
     while (1)
     {
-        if (g_reqSendButton3Msg)
-        {
-            g_reqSendButton3Msg = false;
-            SendCanMessageNonBlocking(TX_MSG_ID_TIMER, 0x33);
-        }
+        /* CRITICAL SECTION: Check FIFO logic atomically */
+        __disable_irq();
+        bool hasData = !FIFO_IsEmpty();
+        __enable_irq();
 
-        if (g_reqSendButton2Msg)
+        /* If we have data AND the previous transfer is done */
+        if (hasData && txComplete)
         {
-            g_reqSendButton2Msg = false;
-            SendCanMessageNonBlocking(TX_MSG_ID_TIMER, 0x22);
-        }
+            /* Pop the message safely */
+            __disable_irq();
+            bool success = FIFO_Pop(&nextMsg);
+            __enable_irq();
 
-        if (g_reqSendTimerMsg)
-        {
-            g_reqSendTimerMsg = false;
-            SendCanMessageNonBlocking(TX_MSG_ID_TIMER, 0xBB);
+            if (success)
+            {
+                SendCanMessageNonBlocking(nextMsg.id, nextMsg.dataByte);
+            }
         }
     }
 }
